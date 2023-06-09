b08850d57bcc5be792fb0e3f3d8ae6d671eb216c
Nam Chan <namanhseoul@gmail.com>
2023-06-09 14:28:43 +0700

    Create 2-Sao chep Mang int 5 phan tu sang Mang float 10 phan tu.c

---
+++ C - Lab 06/2-Sao chep Mang int 5 phan tu sang Mang float 10 phan tu.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(){
+	int intArr[5], i;
+	float floatArr[10];
+	printf("Enter 5 element in to the integer Array: \n");
+	for(i = 0; i < 5; i++){
+		printf("Enter element %d: ", i + 1);
+		scanf("%d", &intArr[i]);
+	}
+	printf("\nInteger Array: ");
+	for(i = 0; i < 5; i++){
+		printf("%d ", intArr[i]);
+	}
+	printf("\n\nFloat Array: ");
+	for(i = 0; i < 10; i++){
+		floatArr[i] = (float)intArr[i];
+		if(i < 5){
+			printf("%0.2f ", floatArr[i]);
+		}
+	}
+	for(i = 5; i < 10; i++){
+		floatArr[i] = 0;
+		printf("%0.2f ", floatArr[i]);
+	}
+	return 0;
+}
