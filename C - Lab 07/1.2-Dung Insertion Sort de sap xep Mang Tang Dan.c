f15d5501311954057d4c1c757feb602799d53f9c
Nam Chan <namanhseoul@gmail.com>
2023-06-11 23:32:54 +0700

    Create 1.2-Dung Insertion Sort de sap xep Mang Tang Dan.c

---
+++ C - Lab 07/1.2-Dung Insertion Sort de sap xep Mang Tang Dan.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+	// Declaration
+	int arr[100];
+	int n, temp;
+	
+	// Input elements
+	printf("Enter number of element: ");
+	scanf("%d", &n);
+	for(int i = 0; i < n; i++){
+		printf("Enter element number %d: ", i + 1);
+		scanf("%d", &arr[i]);
+	}
+	
+	// Insertion sort
+	for(int j = 0; j < n; j++){
+		int i = j - 1;
+		temp = arr[j];
+		while((i >= 0) && (temp < arr[i])){
+			arr[i + 1] = arr[i];
+			i--;
+		}
+		arr[i + 1] = temp;
+	}
+	for(int i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	getch();
+}
