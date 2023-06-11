9c4020e87c75a8b910775ca6bd7508b20dcb27eb
Nam Chan <namanhseoul@gmail.com>
2023-06-11 23:10:37 +0700

    Update 8-Mang 2 chieu luu tru diem 6 mon hoc cua 5 SV nhap vao va tinh diem TB.c

--- C - Lab 06/8-Mang 2 chieu luu tru diem 6 mon hoc cua 5 SV nhap vao va tinh diem TB.c
+++ C - Lab 06/8-Mang 2 chieu luu tru diem 6 mon hoc cua 5 SV nhap vao va tinh diem TB.c
@@ -1,48 +1,43 @@
 #include <stdio.h>
-
-#define NUM_STUDENTS 5
-#define NUM_SUBJECTS 6
-
-int main() {
-    char subjects[NUM_SUBJECTS][100];
-    float scores[NUM_STUDENTS][NUM_SUBJECTS];
-    float average[NUM_STUDENTS] = {0};
+int main(){
+	char subjects[6][100];
+	float scores[5][6];
+	float average[5] = {0};
 	
-    // Nhập tên 6 môn học
-    printf("Nhap ten 6 mon hoc:\n");
-    for(int i = 0; i < NUM_SUBJECTS; i++) {
-        printf("Mon hoc %d: ", i + 1);
+	// Nhập tên 6 môn học
+	printf("Nhap ten 6 mon hoc:\n");
+	for(int i = 0; i < 6; i++){
+		printf("Mon hoc %d: ", i + 1);
 		gets(subjects[i]);
-    }
-
-    // Nhập điểm cho 5 sinh viên
-    for(int i = 0; i < NUM_STUDENTS; i++) {
-        printf("\nNhap diem cho sinh vien %d:\n", i + 1);
-        for(int j = 0; j < NUM_SUBJECTS; j++) {
-            printf("Diem mon hoc %s: ", subjects[j]);
-            scanf("%f", &scores[i][j]);
-            average[i] += scores[i][j];
-        }
-        average[i] /= NUM_SUBJECTS;
-    }
-
-    // Hiển thị bảng lưu trữ điểm
-    printf("\nBang luu tru diem:\n");
-    printf("---------------------------------------------------------------------------------------------------------------------------------\n");
-	printf("|   Sinh vien   |");
-	for(int i = 0; i < NUM_SUBJECTS; i++){
+	}
+	
+	// Nhập điểm 5 sinh viên và tính điểm TB
+	for(int i = 0; i < 5; i++){
+		printf("\nNhap diem cho sinh vien %d:\n", i + 1);
+		for(int j = 0; j < 6; j++){
+			printf("Diem mon %s: ", subjects[j]);
+			scanf("%f", &scores[i][j]);
+			average[i] += scores[i][j];
+		}
+		average[i] /= 6;
+	}
+	
+	// Hiển thị kết quả ra màn hình
+	printf("\nBang luu tru diem:\n");
+	printf("\n---------------------------------------------------------------------------------------------------------------------------------\n");
+	printf("|	Hoc vien|");
+	for(int i = 0; i < 6; i++){
 		printf("	%s	|", subjects[i]);
 	}
 	printf("	TB	|");
 	printf("\n---------------------------------------------------------------------------------------------------------------------------------\n");
-    for (int i = 0; i < NUM_STUDENTS; i++) {
-        printf("|  Sinh vien %d  |", i + 1);
-        for (int j = 0; j < NUM_SUBJECTS; j++) {
-            printf("	%0.2f	|", scores[i][j]);
-        }
-        printf("	%0.2f	|", average[i]);
+	for(int i = 0; i < 5; i++){
+		printf("|	HV %d	|", i + 1);
+		for(int j = 0; j < 6; j++){
+			printf("	%0.2f	|", scores[i][j]);
+		}
+		printf("	%0.2f	|", average[i]);
 		printf("\n---------------------------------------------------------------------------------------------------------------------------------\n");
-    }
-
-    return 0;
+	}
+	return 0;
 }
