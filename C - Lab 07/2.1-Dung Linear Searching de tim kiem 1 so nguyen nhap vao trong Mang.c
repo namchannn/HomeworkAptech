54ed021227b1ca35064f5742d9dfaa36c359243c
Nam Chan <namanhseoul@gmail.com>
2023-06-12 00:30:40 +0700

    Create 2.1-Dung Linear Searching de tim kiem 1 so nguyen nhap vao trong Mang.c

---
+++ C - Lab 07/2.1-Dung Linear Searching de tim kiem 1 so nguyen nhap vao trong Mang.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main(){
+	// Declaration
+	int arr[100];
+	int n = 0;
+	int x;
+	
+	// Input elements
+	while(1){
+		printf("Enter element number %d: ", n + 1);
+		scanf("%d", &arr[n]);
+		if(arr[n] == 0){
+			break;
+		}else{
+			n++;
+		}
+	}
+	
+	// Display entered elements
+	printf("Entered elements: \n");
+	for(int i = 0; i < n; i++){
+		printf("%d ", arr[i]);
+	}
+	
+	// Enter element for searching
+	printf("\nEnter element for searching: ");
+	scanf("%d", &x);
+	
+	// Linear searching
+	int j;
+	for(j = 0; j < n; j++){
+		if(x == arr[j]){
+			break;
+		}
+	}
+	if(j == n){
+		printf("Number %d not found", x);
+	}else{
+		printf("Number %d found at position %d", x, j);
+	}
+	getch();
+}
