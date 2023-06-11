957d7655d65349d25939577c52c7466dd73b9f12
Nam Chan <namanhseoul@gmail.com>
2023-06-12 02:50:04 +0700

    Create 3.2-Tim phan tu chung cua 2 Mang so nguyen cho truoc.c

---
+++ C - Lab 07/3.2-Tim phan tu chung cua 2 Mang so nguyen cho truoc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	// Declaration
+	int arr1[5] = {0,3,6,9,1};
+	int arr2[6] = {1,0,2,6,4,5};
+	
+	// Find same elements
+	printf("Same element: ");
+	for(int i = 0; i < 5; i++){
+		for(int j = 0; j < 6; j++){
+			if(arr1[i] == arr2[j]){
+				printf("%d ", arr1[i]);
+				break;
+			}
+		}
+	}
+	return 0;
+}
