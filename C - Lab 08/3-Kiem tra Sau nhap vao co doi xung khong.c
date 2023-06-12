e35adb9bc5fe955744d4a6bd9fb1d1c2097690fc
Nam Chan <namanhseoul@gmail.com>
2023-06-12 11:00:20 +0700

    Create 3-Kiem tra Sau nhap vao co doi xung khong.c

---
+++ C - Lab 08/3-Kiem tra Sau nhap vao co doi xung khong.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// Creat check function
+bool checkDoiXung(char *str){
+	int i = 0;
+	int j = strlen(str) - 1;
+	while(i < j){
+		if(str[i] != str[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
+}
+
+int main(){
+	// Declaration
+	char str[80];
+	int low = 0;
+	int high = strlen(str) - 1;
+	
+	// Input elements
+	printf("Enter elements: ");
+	scanf("%s", str);
+	
+	// Function call
+	if(checkDoiXung(str)){
+		printf("Chuoi nhap vao la chuoi doi xung.\n");
+	}else{
+		printf("Chuoi nhap vao khong phai la chuoi doi xung.\n");
+	}
+	return 0;
+}
