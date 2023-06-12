c0b2d09a4dd525fdfbe83967b6162433a4201546
Nam Chan <namanhseoul@gmail.com>
2023-06-12 14:30:43 +0700

    Update 7-Viet chuong trinh nhap vao 10 ky tu.c

--- C - Lab 06/7-Viet chuong trinh nhap vao 10 ky tu.c
+++ C - Lab 06/7-Viet chuong trinh nhap vao 10 ky tu.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+
 int main(){
+	
+	// Declaration
 	char c[10];
 	int count = 0, i;
+	
+	// Input
 	printf("Enter not over 10 characters: ");
 	while(count < 10){
 		char ch = getchar();
@@ -11,6 +16,8 @@ int main(){
 		c[count] = ch;
 		count++;
 	}
+	
+	// Display
 	printf("\nList character entered:\n\n");
 	for(i = 0; i < count; i++){
 		printf("Character: %c, Code ASCII: %d\n", c[i], c[i]);
