b0a6833df4bf0670d4087152798df2ceb5b4ae9d
Nam Chan <namanhseoul@gmail.com>
2023-06-12 14:30:35 +0700

    Update 1-Hien thi cac phan tu o vi tri chan.c

--- C - Lab 06/1-Hien thi cac phan tu o vi tri chan.c
+++ C - Lab 06/1-Hien thi cac phan tu o vi tri chan.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
 int main(){
+	
+	// Declaration
 	int arr[10], i;
+	
+	// Input
 	printf("Enter 10 element to the array: ");
 	for(i = 0; i < 10; i++){
 		scanf("%d", &arr[i]);
 	}
 	printf("\n");
+	
+	// Display
 	for(i = 0; i < 10; i++){
 		if(i % 2 == 0){
 			printf("%d ", arr[i]);
