9af1053ef4e6f45294a4610c76a0464a54fd6543
Nam Chan <namanhseoul@gmail.com>
2023-06-12 14:30:42 +0700

    Update 6-Hien thi ten 5 sinh vien nhap vao toi da 30 ky tu.c

--- C - Lab 06/6-Hien thi ten 5 sinh vien nhap vao toi da 30 ky tu.c
+++ C - Lab 06/6-Hien thi ten 5 sinh vien nhap vao toi da 30 ky tu.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 int main(){
+	
+	// Declaration
 	char name[5][30];
 	int i;
+	
+	// Input
 	for(i = 0; i < 5; i++){
 		printf("Enter name: ");
 		gets(name[i]);
 	}
 	printf("\n");
+	
+	// Display
 	for(i = 0; i < 5; i++){
 		puts(name[i]);
 	}
