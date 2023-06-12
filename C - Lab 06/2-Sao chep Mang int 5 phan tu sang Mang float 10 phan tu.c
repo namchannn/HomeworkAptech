9a6768c94658c6f133763929dafec2501c76de92
Nam Chan <namanhseoul@gmail.com>
2023-06-12 14:30:37 +0700

    Update 2-Sao chep Mang int 5 phan tu sang Mang float 10 phan tu.c

--- C - Lab 06/2-Sao chep Mang int 5 phan tu sang Mang float 10 phan tu.c
+++ C - Lab 06/2-Sao chep Mang int 5 phan tu sang Mang float 10 phan tu.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
+
 int main(){
+	
+	// Declaration
 	int intArr[5], i;
 	float floatArr[10];
+	
+	// Input
 	printf("Enter 5 element in to the integer Array: \n");
 	for(i = 0; i < 5; i++){
 		printf("Enter element %d: ", i + 1);
 		scanf("%d", &intArr[i]);
 	}
+	
+	// Display
 	printf("\nInteger Array: ");
 	for(i = 0; i < 5; i++){
 		printf("%d ", intArr[i]);
