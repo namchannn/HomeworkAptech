ef1474895f011ec95b069c882215c14d3e2883e9
Nam Chan <namanhseoul@gmail.com>
2023-06-12 11:00:18 +0700

    Create 2-Nhap Mang so nguyen 5 phan tu bang Con Tro.c

---
+++ C - Lab 08/2-Nhap Mang so nguyen 5 phan tu bang Con Tro.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	// Declaration
+	int arr[5];
+	int *ptr;
+	ptr = (int*)malloc(5*sizeof(int));
+	
+	// Input elements
+	for(int i = 0; i < 5; i++){
+		printf("Enter element number %d: ", i + 1);
+		scanf("%d", &ptr[i]);
+	}
+	
+	// Display entered elements
+	printf("Entered elements: \n");
+	for(int i = 0; i < 5; i++){
+		printf("%d ", ptr[i]);
+	}
+	return 0;
+}
