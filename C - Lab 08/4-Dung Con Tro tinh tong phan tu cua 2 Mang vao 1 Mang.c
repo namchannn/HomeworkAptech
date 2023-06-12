0021d6a488e95387ccf773c56138b3d7660bdc18
Nam Chan <namanhseoul@gmail.com>
2023-06-12 12:31:38 +0700

    Update 4-Dung Con Tro tinh tong phan tu cua 2 Mang vao 1 Mang.c

--- C - Lab 08/4-Dung Con Tro tinh tong phan tu cua 2 Mang vao 1 Mang.c
+++ C - Lab 08/4-Dung Con Tro tinh tong phan tu cua 2 Mang vao 1 Mang.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void tinhTong(int *a, int *b, int *c, int n){
 	for(int i = 0; i < n; i++){
@@ -12,10 +13,14 @@ void inMang(int *mang, int n){
 	}
 	printf("\n");
 }
+
 int main(){
 	// Declaration
 	int a[5], b[5], c[5];
-	
+	int *ptr1, *ptr2, *ptr3;
+	ptr1 = (int*)malloc(5*sizeof(int));
+	ptr2 = (int*)malloc(5*sizeof(int));
+	ptr3 = (int*)malloc(5*sizeof(int));
 	// Input elements
 	printf("Enter elements for A: ");
 	for(int i = 0; i < 5; i++){
@@ -25,12 +30,20 @@ int main(){
 	for(int i = 0; i < 5; i++){
 		scanf("%d", &b[i]);
 	}
-	tinhTong(a, b, c, 5);
+	for(int i = 0; i < 5; i++){	//tinhTong(a, b, c, 5);
+		c[i] = a[i] + b[i];
+	}
 	printf("\nMang A: ");
-	inMang(a, 5);
-	printf("Mang B: ");
-	inMang(b, 5);
-	printf("Mang C: ");
-	inMang(c, 5);
+	for(int i = 0; i < 5; i++){ //inMang(a, 5);
+		printf("%d ", a[i]);
+	}
+	printf("\nMang B: ");
+	for(int i = 0; i < 5; i++){ //inMang(b, 5);
+		printf("%d ", b[i]);
+	} 
+	printf("\nMang C: ");
+	for(int i = 0; i < 5; i++){ //inMang(c, 5);
+		printf("%d ", c[i]);
+	} 
 	return 0;
 }
