18811ab6199af98d5315b5c51942c44428362e56
Nam Chan <namanhseoul@gmail.com>
2023-06-13 17:03:09 +0700

    Update 2-Viet Ham nhap va check so nguyen to.c

--- C - Lab 09/2-Viet Ham nhap va check so nguyen to.c
+++ C - Lab 09/2-Viet Ham nhap va check so nguyen to.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
 
-int inputNumber(int num){
+int inputNumber(){
+	int num;
 	printf("Enter checking number: ");
 	scanf("%d", &num);
 	return num;
 }
 
 void checkNumber(int num){
-	int flag = 0;
-	if(num == 0 || num == 1){
-		flag = 1;
-	}
-	for(int i = 2; i < num / 2; i++){
-		if(num % i == 0){
-			flag = 1;
-			break;
-		}
-	}
-	if(flag == 1){
-		printf("Number %d is not Prime number.", num);
-	}else{
-		printf("Number %d is Prime number.", num);
-	}
+	int flag = 1;
+	
+    if(num < 2){
+        flag = 0;
+    }else{
+        for(int i = 2; i <= num / 2; i++){
+            if(num % i == 0){
+                flag = 0;
+                break;
+            }
+        }
+    }
+    if(flag) {
+        printf("So %d la so nguyen to.\n", num);
+    }else{
+        printf("So %d khong phai so nguyen to.\n", num);
+    }
 }
 
 int main(){
-	int num = inputNumber(num);
-	checkNumber(num);
+	char choice;
+	do{
+		int num = inputNumber(num);
+		checkNumber(num);
+		
+		printf("Ban co muon tiep tuc khong? (y/n): ");
+		fflush(stdin);
+		scanf("%c", &choice);
+	}while(choice == 'y' || choice == 'Y');
+	
 	return 0;
 }
